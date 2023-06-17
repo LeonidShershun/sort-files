14739d0ebb9719f0c78e772fe58cf21ed31654f0
Леонід Шершун <131890552+LeonidShershun@users.noreply.github.com>
2023-06-18 02:29:48 +0300

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+from pathlib import Path
 
 
 baza_for_filtr = {
@@ -12,7 +13,7 @@ baza_for_filtr = {
 }
 
 
-# main_path = r'C:\Users\LeonShell\Desktop\test_2dz – копія – копія'
+#main_path = r'C:\Users\LeonShell\Desktop\test_2dz – копія – копія'
 
 
 baz_fold = []
@@ -47,16 +48,16 @@ def create_folders_from_list(folder_path, folder_names):
 
 
 
-def get_Folder_list(folders_paths, excluz):
+def get_Folder_list(folders_paths, excluz): 
     pathess = os.scandir(folders_paths)
-    for path in pathess:
-        if path.path not in excluz:
-            if path.is_file():
-                baz_file.append(path.path)
-            if path.is_dir():
-                baz_fold.append(path.path)
-                sub_path = os.scandir(path)
-                get_Folder_list(path.path, excluz)
+    for fh in pathess:
+        if fh.path not in excluz:
+            if fh.is_file():
+                baz_file.append(fh.path)
+            if fh.is_dir():
+                baz_fold.append(fh.path)
+                sub_path = os.scandir(fh)
+                get_Folder_list(fh.path, excluz)
     
     return  baz_file, baz_fold
 
@@ -64,41 +65,34 @@ def get_Folder_list(folders_paths, excluz):
 
 def get_rename_files(files):
     new_list_files = []
-    exe_list_in_baza = list(baza_for_filtr.items()) 
     for f in files:
         for name in f:
             if '.' in name:
                 name_ = name.split('\\')[-1]
                 name_path = name.split(name_)[-2]
-                new_name = name_path  + normalize(name_)
+                new_name = name_path + normalize(name_)
                 os.rename(name, new_name)
                 new_list_files.append(new_name) 
     return new_list_files
 
 
-
-def get_move_files(files):
-    a = get_Folder_list(main_path, create_folders_from_list(main_path, baza_for_filtr))
+list_remove_file = []
+def get_move_files(files, main_path):
     exe_list_in_baza = list(baza_for_filtr.items())
     adrenal = 0
     for name in files:
         name_exe = name.split('.')[-1]
         name_ = name.split('\\')[-1]
         name_path = name.split(name_)[-2]
-        for dict_key_int in range(len(exe_list_in_baza)):
-            if name_exe.upper() in exe_list_in_baza[dict_key_int][1]:
-                new_name = main_path + f"\{exe_list_in_baza[dict_key_int][0]}\\" + name_
-                try:    
-                    shutil.move(f'{name_path}\{name_}', f'{main_path}\{exe_list_in_baza[dict_key_int][0]}\{name_}')
-                except:
-                    if FileNotFoundError:
-                        adrenal = 1
-                        continue
-                        
-                    if RecursionError:
-                        adrenal = 1
-                        continue
-                        
+        for fold, exes in exe_list_in_baza:
+            if name_exe.upper() in exes:
+                list_remove_file.append(name)
+                shutil.move(f'{name_path}\{name_}', f'{main_path}\{fold}\{name_}')
+    go = list(set(files)-set(list_remove_file))
+    for name in go:
+        name_ = name.split('\\')[-1]
+        shutil.move(f'{name}', f'{main_path}\{name_}')
+           
     return adrenal
 
 
@@ -119,40 +113,25 @@ def remove_empty_folders(baz_files):
        
         
 # Розпакування архівів
-def unpack_archives(catalog):
+def unpack_archives(catalog, main_path):
     for arch_name in catalog:
         if arch_name.split('\\')[-1] == 'archives':
             arch_list  = os.scandir(arch_name)
             for arch in arch_list:
-                arch_name = arch.path
-                arch_name = str(arch_name)
-                arch_name = arch_name.replace("['", '')
-                arch_name = arch_name.replace("']", '')
-                direct = arch_name.split('\\')[-2]
-                name_for_move = arch_name.split('\\')[-1]
-                folder_for_unpack = arch_name.split('\\')[-1]
-                folder_for_unpack = folder_for_unpack.split('.')[-2]
-                shutil.unpack_archive(arch_name, rf'{main_path}\{direct}\{folder_for_unpack}')
-                os.rename(arch_name, f'{main_path}\{direct}\{folder_for_unpack}\{name_for_move}')
+                if '.' in arch.path:
+                    arch_name = arch.path
+                    arch_name = str(arch_name)
+                    arch_name = arch_name.replace("['", '')
+                    arch_name = arch_name.replace("']", '')
+                    direct = arch_name.split('\\')[-2]
+                    name_for_move = arch_name.split('\\')[-1]
+                    folder_for_unpack = name_for_move.split('.')[-2]
+                    shutil.unpack_archive(arch_name, rf'{main_path}\{direct}\{folder_for_unpack}')
+                    os.rename(arch_name, f'{main_path}\{direct}\{folder_for_unpack}\{name_for_move}')
                 return print('Ok!')
             
 
 
-# Пеейменування папок
-def get_rename_folders(folder_path):
-    rename_list =[]
-    for data in folder_path:
-        for folder in data:
-            if '.' not in folder:
-                if folder not in rename_list:
-                    rename_list.append(folder)
-        rename_list.reverse()
-        for folder in rename_list:
-            name_folder = folder.split('\\')[-1]
-            print(name_folder)
-            patch_folder = folder.split(name_folder)[-2]
-            print(patch_folder)
-            os.rename(folder, f'{patch_folder}{normalize(name_folder)}')
 
 
 
@@ -160,17 +139,32 @@ def get_sort(folder_path):
     excluz = create_folders_from_list(folder_path, baza_for_filtr)
     baz_files = get_Folder_list(folder_path, excluz)
     new_baz_file = get_rename_files(baz_files)
-    adrenal = get_move_files(new_baz_file)
+    adrenal = get_move_files(new_baz_file, folder_path)
     if adrenal >= 1:
-        adrenal = get_move_files(new_baz_file)
+        adrenal = get_move_files(new_baz_file, folder_path)
     remove_empty_folders(baz_files)
-    unpack_archives(excluz)
-    get_rename_folders(baz_files)
+    unpack_archives(excluz, folder_path)
 
 
 
 
-#main_path = input('Введіть адресу папки дя сортування файлів: ')
-main_path = sys.argv[1]
-get_sort(main_path)
-print()
+# #main_path = sys.argv[1]
+# get_sort(main_path)
+# print()
+def main():
+    try:
+        path = Path(sys.argv[1])
+    except IndexError:
+        return "Не вказано адресу теки для сортквання."
+    
+    if not path.exists():
+        return f"Теки з назвою {path} не знайдено."
+    
+    get_sort(path)
+    
+    
+    return "Все гаразд, сортування завершено!"
+
+
+if __name__ == "__main__":
+    print(main())
